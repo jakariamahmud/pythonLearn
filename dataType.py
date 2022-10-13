97a5062c453525af235247b5a722193e1ec324a5
jakaria-mahmud <jakaria0499@gmail.com>
2022-10-13 12:33:10 +0600

    data type is added

---
+++ dataType.py
@@ -0,0 +1,78 @@
+# here is a discussion about python data types
+# 1. text type:-  str
+x = str("jakaria mahmud")
+print(x)
+print("--------------------------------------------")
+
+# 2. numeric types:- int, float, complex
+x = int(20)
+print(x)
+print("--------------------------------------------")
+
+x = float(20.0)
+print(x)
+print("--------------------------------------------")
+
+x = complex(1j)
+print(x)
+print("--------------------------------------------")
+
+
+# 3. sequence types:- list, tuple, range
+x = list(("apple", "banana", "cherry"))
+print(x)
+print("--------------------------------------------")
+
+x = tuple(("apple", "banana", "cherry"))
+print(x)
+print("--------------------------------------------")
+
+x = range(6)
+print(x)
+print("--------------------------------------------")
+
+# 4. mapping types:- dist
+x = dict(name="jakaria", age=22)
+print(x)
+print("--------------------------------------------")
+
+# 5. set types:- set, frozenset
+x = set(("apple", "banana", "cherry"))
+print(x)
+print("--------------------------------------------")
+x = frozenset(("apple", "banana", "cherry"))
+print(x)
+print("--------------------------------------------")
+
+
+# 6. boolean types:- bool
+x = bool(5)
+print(x)
+print("--------------------------------------------")
+
+
+# 7. binary tyjpes:- bytes, bytearray, memoryview
+x = bytes(5)
+print(x)
+print("--------------------------------------------")
+
+x = bytearray(5)
+print(x)
+print("--------------------------------------------")
+
+x = memoryview(bytes(5))
+print(x)
+print("--------------------------------------------")
+
+# 8. none type:- noneType
+
+
+
+
+##### another discution of data type
+# complax number are written with a "j" as the imaginary part:
+
+
+
+import random
+print(random.randrange(1, 100000))
